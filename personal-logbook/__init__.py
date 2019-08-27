0af3b3e1b493dcce5a0fc60fbb513844bd8ebd15
Nicola Zanardi <nicola.zanardi@energy21.com>
2019-08-27 17:01:55 +0200

    Initial web interface implementation

--- personal-logbook/__init__.py
+++ personal-logbook/__init__.py
@@ -1,21 +1,28 @@
 from model import LogItem, Item
 from fastapi import FastAPI
 import uvicorn
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.post("/logs")
+@app.post("/logs", status_code=201)
 async def store_log(item: Item):
-    print(item.text)
+    print(item)
     item = LogItem(content=item.text)
     item.save()
+    return item
 
 
 @app.get("/logs")
 async def get_logs():
-    items = list(LogItem.select())
+    items = list(LogItem.select().order_by(LogItem.id.desc()))
     return items
 
+@app.get("/", response_class=HTMLResponse)
+async def read_items():
+    with open("view/index.html") as html_index:
+        return html_index.read()
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
