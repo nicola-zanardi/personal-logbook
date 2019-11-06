8418dfc36568c9bfe82d4c8d6545fa30ea9c3cc1
nicola-zen <mildlyinteresting@posteo.me>
2019-11-06 16:28:58 +0100

    First implementation edit and delete funcitonality
    -> have to figure out base URL now
    -> have to come up with a way of abstracting the base template from the sub ones

--- logbook/controllers.py
+++ logbook/controllers.py
@@ -17,6 +17,7 @@ def get_logs(page=1):
     response = []
     for item in items:
         row = {}
+        row["id"] = item.id
         row[
             "timestamp"
         ] = f'{item.timestamp.strftime("%m-%d-%Y %H:%M:%S")} ({humanize.naturaldelta(item.timestamp)} ago)'
@@ -27,3 +28,26 @@ def get_logs(page=1):
         )
         response.append(row)
     return response
+
+
+def get_log_content(log_id):
+    log = LogItem.get(LogItem.id == log_id)
+    return log.content
+
+
+@app.route("/logs/<log_id>/edit", methods=["POST"])
+def update_log_content(log_id):
+    new_content = request.form["text"]
+    log = LogItem.get(LogItem.id == log_id)
+    log.content = new_content
+    log.save()
+    return redirect("/")
+
+
+# deleting with a post as forms do not support DELETE and I
+# am trying to stay javascript-free
+@app.route("/logs/<log_id>/delete", methods=["POST"])
+def delete_log_entry(log_id):
+    log = LogItem.get(LogItem.id == log_id)
+    log.delete_instance()
+    return redirect("/")
--- logbook/views.py
+++ logbook/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
 
 from logbook import app
-from logbook.controllers import get_logs, store_log
+from logbook.controllers import get_logs, store_log, get_log_content
+from logbook.models import LogItem
 
 
 @app.route("/", defaults={"page_nr": 1})
@@ -9,3 +10,10 @@ from logbook.controllers import get_logs, store_log
 def index_next_pages(page_nr):
     items = get_logs(page_nr)
     return render_template("index.html", items=items, page_nr=int(page_nr))
+
+
+@app.route("/logs/<log_id>/edit", methods=["GET"])
+def get_log_entry(log_id):
+    content = get_log_content(log_id)
+    return render_template("edit_log.html", content=content, log_id=int(log_id))
+
