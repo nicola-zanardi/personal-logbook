dd699f069289b90f5f8ddb8707f6ebf2c6b8d747
Nicola Zanardi <mildlyinteresting@posteo.me>
2020-01-06 18:27:05 +0100

    Added debug mode CLI parameter

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import sys
+
 from logbook import app
 
 if __name__ == "__main__":
-    app.run("127.0.0.1", port=5000, debug=True)
+    app.run("127.0.0.1", port=5000, debug="--debug" in sys.argv)
