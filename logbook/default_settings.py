51f7d9bd63a65bb8ac5a20a57cf0d3f50d145e0c
nicola-zen <mildlyinteresting@posteo.me>
2020-01-08 08:35:30 +0100

    Unstyled implementation of login screen

--- logbook/default_settings.py
+++ logbook/default_settings.py
@@ -1,2 +1,3 @@
 DEBUG = False  # make sure DEBUG is off unless enabled explicitly otherwise
-LOG_DIR = '.'  # create log files in current working directory
+LOG_DIR = "."  # create log files in current working directory
+SECRET_KEY = "9OLWxND4o83j4K4iuopO"
