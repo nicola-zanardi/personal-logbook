72177a3d792c73d4a89c022ec61c246fe71afb37
nicola.zanardi <nicola.zanardi@energy21.com>
2020-01-15 09:59:47 +0100

    Reimplement exisiting + auth UI using tailwindcss

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 import sys
+import os
+import subprocess
 
 from logbook import app
 
+DEBUG_MODE = "--debug" in sys.argv
+
+env = os.environ
+if not DEBUG_MODE:
+    env = {**os.environ, "NODE_ENV": "production"}
+
+subprocess.Popen("npm run build-css", env=env, shell=True).wait()
+
 if __name__ == "__main__":
-    app.run("127.0.0.1", port=5000, debug="--debug" in sys.argv)
+    app.run("127.0.0.1", port=5000, debug=DEBUG_MODE)
